214c63da5f08043fe781b1278cb073676df59600
sallliisa <sodiumcation@gmail.com>
2023-07-14 19:03:39 +0700

    gamer

--- src/lib/components/motion/Fade.svelte
+++ src/lib/components/motion/Fade.svelte
@@ -1,11 +1,13 @@
 <script>
   import { motion } from "@/lib/app/motion"
   import { fade } from "@/lib/app/motion/animate"
+  import { easing } from "@/lib/app/motion/easing"
   import { location } from "svelte-spa-router"
 
 </script>
+
 {#key $location}
-<div in:fade={{duration: motion.duration.short2, delay: motion.duration.short2}} out:fade={{duration: motion.duration.short2}}>
-  <slot/>
-</div>
+  <div in:fade={{duration: motion.duration.short2, easing: motion.easing.legacy, delay: motion.duration.short2}} out:fade={{duration: motion.duration.short2, easing: motion.easing.legacy}}>
+    <slot/>
+  </div>
 {/key}
